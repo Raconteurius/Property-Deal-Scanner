f7eb62baa5fdc52031ac392a22737212ec00c95d
REvilo <ojbland@gmail.com>
2026-03-09 20:19:00 +0000

    upgrade otm schema with scoring fields

--- api/refresh-otm.js
+++ api/refresh-otm.js
@@ -5,18 +5,31 @@ module.exports = async (req, res) => {
     const listings = {
       results: [
         {
-          id: "otm_live_1",
-          title: "Bedminster terrace",
-          address: "Bedminster, Bristol",
-          price: 395000,
-          beds: 3,
-          type: "Terrace",
-          source: "OnTheMarket",
-          link: "https://www.onthemarket.com/",
-          image: "https://images.unsplash.com/photo-1560184897-ae75f418493e",
-          summary: "Refurb opportunity with good margin",
-          tags: ["Flip", "Works"],
-          score: 95
+      id: "otm_live_1",
+      title: "Bedminster terrace",
+      address: "Bedminster, Bristol",
+      price: 395000,
+      estimatedValue: 460000,
+      refurbCost: 35000,
+      beds: 3,
+      type: "Terrace",
+      daysOnMarket: 42,
+      priceDrop: true,
+
+      source: "OnTheMarket",
+      link: "https://www.onthemarket.com/",
+      image: "https://images.unsplash.com/photo-1560184897-ae75f418493e",
+
+      tags: ["Flip", "Works"],
+
+      scores: {
+        bmv: 88,
+        flip: 92,
+        hmo: 55,
+        master: 86
+      },
+
+      summary: "Strong refurb opportunity with margin"
         }
       ]
     }
